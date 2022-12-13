e7db2dee3148c3a697839615378c6ed391f63422
Kate <katewolshanat@gmail.com>
2022-12-13 13:59:49 +0400

    first task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,47 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let equationOne;
+  let equationTwo;
+  let D = Math.pow(b, 2) - 4 * a * c;
+
+  if (D < 0){
+    console.log("Корней нет");
+  }
+  else if (D === 0){
+    equationOne = -b / (2*a);
+    arr.push(equationOne);
+    console.log(arr);
+  }
+  else{
+    equationOne = (-b + Math.sqrt(D))/ (2*a);
+    equationTwo = (-b - Math.sqrt(D))/ (2*a);
+    arr.push(equationOne);
+    arr.push(equationTwo);
+    console.log("Корень 1 = " + arr[0] + ";" +  "Корень 2: " + arr[1]);
+  }
+
+  return(arr);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let bodyCredit;
+  let monthPayment;
+  let allPayment;
+
+  if (
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    isNaN(countMonths) 
+  ) {
+    console.log("Одно из параметров неверно")
+    return false;
+  }
+    percent = (percent / 100) / 12;
+    bodyCredit = amount - contribution;
+    monthPayment = bodyCredit * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+    allPayment = parseFloat((monthPayment * countMonths).toFixed(2));
+    
+    return allPayment;
 }
