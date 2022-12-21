bd7636814b0ae4597c07e0bc15de9dc80f3ca1ab
Kate <katewolshanat@gmail.com>
2022-12-21 11:48:54 +0100

    Task three completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users
+        .filter(user => user.gender === gender)
+        .map(result => result.age)
+        .reduce((acc, item, index, arr) => {
+            acc += item
+            if (index === arr.length - 1) {
+                return acc / arr.length;
+            }
+            return acc;
+        }, 0);
 }
